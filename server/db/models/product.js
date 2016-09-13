8525af3b1d50e9ac0174e8cb4f82e3c7878318d3
Han Hyuk Cho <hayden83.cho@gmail.com>
2016-09-13 10:37:45 -0400

    product schema updated

---
+++ server/db/models/product.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+var db = require('../_db');
+
+
+module.exports = db.define('product', {
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	category: {
+		type: Sequelize.ENUM(['Rock', 'Mineral', 'Meteorite', 'Fossil', 'etc']),
+		allowNull: false
+	},
+	pictureURL: {
+		type: Sequelize.STRING
+	},
+	price: {
+		type: Sequelize.FLOAT,
+		allowNull: false,
+		validate: {
+			min: 1.00
+		}
+	},
+	description: {
+		type: Sequelize.TEXT
+	},
+	rarity: {
+		type: Sequelize.FLOAT
+	},
+	location: {
+		type: Sequelize.STRING
+	},
+	stocks: {
+		type: Sequelize.INTEGER
+	}
+}, {
+	getterMethods: {
+		snippet: function(){
+			return this.description.slice(0, 15) + '...';
+		},
+		displayPrice: function(){
+			var priceString = this.price.toString();
+			return '$' + priceString;
+		}
+	}
+});
