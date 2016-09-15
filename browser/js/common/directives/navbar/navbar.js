d9f9667a03d2d0a36463fe25c928e91aea00c492
Bruno Raposo <braposo@g.harvard.edu>
2016-09-15 13:38:05 -0400

    home page lookin fre$$hhhh

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -10,7 +10,7 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
                 { label: 'Home', state: 'home' },
                 { label: 'About', state: 'about' },
                 { label: 'Catalog', state: 'catalog' },
-                { label: 'Documentation', state: 'docs' },
+                // { label: 'Documentation', state: 'docs' },
                 { label: 'Members Only', state: 'membersOnly', auth: true },
                 { label: 'Cart', state: 'cart'}
             ];
