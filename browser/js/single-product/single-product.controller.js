fb2b299ab207a2bfd576985a1abcf9ba8b9d45d1
Ten <tenloh@gmail.com>
2016-09-16 14:50:13 -0400

    adding products works

--- browser/js/single-product/single-product.controller.js
+++ browser/js/single-product/single-product.controller.js
@@ -1,14 +1,14 @@
-app.controller('SingleProductCtrl', function($scope, $state, $http, theProduct){
+app.controller('SingleProductCtrl', function ($scope, $state, $http, theProduct, CartFactory) {
 	$scope.product = theProduct;
 	$scope.isWritingAReview = false;
 	$scope.isEditingAReview = false;
 
 	$http.get('/api/reviews/products/' + theProduct.id)
-		.then(function(reviews){
+		.then(function (reviews) {
 			//array of reviews fetched from the server
 			$scope.reviews = reviews.data;
 			var total = 0;
-			$scope.reviews.forEach(function(review){
+			$scope.reviews.forEach(function (review) {
 				total += +review.stars;
 			});
 
@@ -16,45 +16,45 @@ app.controller('SingleProductCtrl', function($scope, $state, $http, theProduct){
 		});
 
 
-		//review stars
-		$scope.maxRating = 5;
+	//review stars
+	$scope.maxRating = 5;
     $scope.ratedBy = 0;
     $scope.rateBy = function (star) {
         $scope.ratedBy = star;
     };
     ////////////////
 
-	$scope.openReviewCreate = function(){
+	$scope.openReviewCreate = function () {
 		$scope.isWritingAReview = true;
 	};
 
-	$scope.createAReview = function(content, stars){
+	$scope.createAReview = function (content, stars) {
 
 		var reviewObj = {
 			reviewContent: content,
 			stars: stars
 		};
 
-		if($scope.isWritingAReview && !$scope.isEditingAReview){
+		if ($scope.isWritingAReview && !$scope.isEditingAReview) {
 			$http.post('/api/reviews/products/' + theProduct.id, reviewObj)
-			.then(function(createdReview){	
-				$state.reload();
-			});	
+				.then(function (createdReview) {
+					$state.reload();
+				});
 		}
 
-		if($scope.isWritingAReview && $scope.isEditingAReview){
+		if ($scope.isWritingAReview && $scope.isEditingAReview) {
 			$http.put('/api/reviews/' + $scope.reviewId, reviewObj)
-			.then(function(){
-				$scope.reviewId = null;
-				$scope.reviewContent = null;
-				$state.reload();
-			})
+				.then(function () {
+					$scope.reviewId = null;
+					$scope.reviewContent = null;
+					$state.reload();
+				})
 		}
-		
+
 	};
 
-	
-	$scope.editReview = function(review){
+
+	$scope.editReview = function (review) {
 		$scope.isWritingAReview = true;
 		$scope.isEditingAReview = true;
 		$scope.reviewContent = review.reviewContent;
@@ -63,13 +63,21 @@ app.controller('SingleProductCtrl', function($scope, $state, $http, theProduct){
 
 	};
 
-	$scope.deleteReview = function(review){
+	$scope.deleteReview = function (review) {
 		$http.delete('/api/reviews/' + review.id)
-			.then(function(){
+			.then(function () {
 				console.log('successfully deleted');
 				$state.reload();
 			});
 	};
+
+	$scope.addToCart = function (product) {
+		CartFactory.addItemToCart(product.id, product.price)
+			.then(function (cart) {
+				$state.go('cart');
+				return cart;
+			}).catch(console.error.bind(console));
+	}
 
 
 })
