568b3501575a9bc2d0fd79ec80cd1c6ce68b177d
Nathan Wallace <ncw232@gmail.com>
2016-09-16 13:02:55 -0400

    sort functionality

--- browser/js/catalog/catalog.controller.js
+++ browser/js/catalog/catalog.controller.js
@@ -9,8 +9,8 @@ app.controller('CatalogCtrl', function($scope, $http, $state, CartFactory, Catal
 		// $scope.rarity = $scope.products.rarity
 			
 	});
-	console.log($scope.all);
 	$scope.categories=["All", "Mineral", "Meteorite", "Rock"];
+	$scope.sortBy=['- Select Sort -', 'price', 'rarity', 'stocks',];
 	$scope.detail = function(product){
 		$state.go('singleproduct', {productID: product.id});
 	}
@@ -23,9 +23,5 @@ app.controller('CatalogCtrl', function($scope, $http, $state, CartFactory, Catal
 		$scope.products=$scope.products.filter(function(product){
 			return product.category===cat;
 		})
-		// CatalogFactory.getAll()
-		// .then(function(products){
-		// 	$scope.products=products;
-		// })
 	}
 })
