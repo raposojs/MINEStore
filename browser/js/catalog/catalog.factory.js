073e09d2dfb8210c85431d0b3725665d6c279bb8
Bruno Raposo <braposo@g.harvard.edu>
2016-09-17 12:54:27 -0400

    fixing some stuff

--- browser/js/catalog/catalog.factory.js
+++ browser/js/catalog/catalog.factory.js
@@ -1,10 +1,10 @@
 app.factory("CatalogFactory", function($http){
 	return {
-		getAll: function(){
-		return $http.get('/api/products')
-		.then(function(products){
-			return products.data;
-		})
+			getAll: function(){
+			return $http.get('/api/products')
+			.then(function(products){
+				return products.data;
+			})
 		}
 	}
 })
